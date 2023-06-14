862461e0d9aff81b4845f56a7cd867e7900ef8f8
xFlippy <94675097+xflipperkast@users.noreply.github.com>
2023-06-14 11:10:33 +0200

    Update flappy.js

--- frontend/js/flappy.js
+++ frontend/js/flappy.js
@@ -245,8 +245,8 @@ function update() {
         if (birdY > 480|| 
           (obstacleX[i] < birdX + 20 && obstacleX[i] + 50 > birdX && 
           (birdY < obstacleTopHeight[i] || birdY + 20 > obstacleTopHeight[i] + gapHeight))) {
-            checkMaxScoreCookie(scoreData.getScore());
-            gameOverTag.innerHTML = "Game Over. Score: " + scoreData.getScore() + "<br> High Score: " + scoreData.getScore();
+            checkMaxScoreCookie(getCookieData("maxScore"));
+            gameOverTag.innerHTML = "Game Over. Score: " + scoreData.getScore() + "<br> High Score: " + getCookieData("maxScore");
             gameOver();
         }
 
